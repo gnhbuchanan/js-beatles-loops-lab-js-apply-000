f134416bcd07e271423c5ba549c228862e519355
Gustaf Buchanan <gnhbuchanan@gmail.com>
2019-05-17 21:44:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,18 @@ function theBeatlesPlay(mems,instruments){
 
 function johnLennonFacts(arr){
   
+  let result = []
   
+  while(result.length < arr.length){
+    
+    let i = 0; 
+    let words = arr[i] + '!!!';
+    result.push(words);
+    i++;
+    
+  }
   
+  return result; 
   
 }
 
